6d52ec329625007d83bc30211f386ef7ff6184a5
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-04 17:45:13 +0300

    Интент "Отправка отчета"

--- app/src/main/java/ru/bupyc9/implictintent/MainActivity.java
+++ app/src/main/java/ru/bupyc9/implictintent/MainActivity.java
@@ -1,7 +1,9 @@
 package ru.bupyc9.implictintent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,19 @@ public class MainActivity extends AppCompatActivity {
         mContactsButton = (Button) findViewById(R.id.button);
         mContactsButton = (Button) findViewById(R.id.button2);
 
+        mContactsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i
+                    .setType("text/plain")
+                    .putExtra(Intent.EXTRA_TEXT, "test")
+                    .putExtra(Intent.EXTRA_SUBJECT, "subject");
 
+                i = Intent.createChooser(i, "выбери меня");
+
+                startActivity(i);
+            }
+        });
     }
 }
