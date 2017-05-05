ca05dd9ee4dbcc452a03093e0736ba5bc994dd59
Afanasyev Pavel <afanasev@worksolutions.ru>
2017-05-05 17:12:40 +0300

    Обработка полученного интента

--- app/src/main/java/ru/bupyc9/implictintent/MainActivity.java
+++ app/src/main/java/ru/bupyc9/implictintent/MainActivity.java
@@ -1,14 +1,21 @@
 package ru.bupyc9.implictintent;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mContactsButton;
     private Button mSendTextButton;
+    private TextView mContactView;
+
+    private static final int REQUEST_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +23,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mContactsButton = (Button) findViewById(R.id.button);
-        mContactsButton = (Button) findViewById(R.id.button2);
+        mSendTextButton = (Button) findViewById(R.id.button2);
+        mContactView = (TextView) findViewById(R.id.textView);
+
+        final Intent pickContact = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
 
         mContactsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(pickContact, REQUEST_CONTACT);
+            }
+        });
+
+        mSendTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Intent.ACTION_SEND);
@@ -32,5 +49,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CONTACT && data != null) {
+            Uri contactUri = data.getData();
+
+            String[] queryFields = new String[] {
+                ContactsContract.Contacts.DISPLAY_NAME
+            };
+
+            Cursor c = getContentResolver().query(contactUri, queryFields, null, null, null);
+
+            try {
+                if (c.getCount() == 0) {
+                    return;
+                }
+
+                c.moveToFirst();
+                String suspect = c.getString(0);
+                mContactView.setText(suspect);
+            } finally {
+                c.close();
+            }
+
+        }
     }
 }
